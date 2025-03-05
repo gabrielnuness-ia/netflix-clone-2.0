c94c1e997e8f20885ef03498b55658d7006d0fe0
gabrielnuness-ia <gabrielnunessads@gmail.com>
2025-03-04 22:11:20 -0300

    Não funcionou mais uma vez. Voltando ao código original

--- public/scripts/api.js
+++ public/scripts/api.js
@@ -1,5 +1,4 @@
-// Configuração da API do The Movie Database
-const API_KEY = "d19a1946970f98fae002af7545322879";
-const IMG_URL = "https://image.tmdb.org/t/p/w500";
-const GENRES_LIST_HTTP = "https://api.themoviedb.org/3/genre/movie/list?";
-const MOVIE_GENRES_HTTP = "https://api.themoviedb.org/3/discover/movie?";
+const api_key = "d19a1946970f98fae002af7545322879"
+const img_url = "https://image.tmdb.org/t/p/w500"
+const genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?"
+const movie_genres_http = "https://api.themoviedb.org/3/discover/movie?"
